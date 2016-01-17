fa3f692080420ce3205ee1f552a69c9eb448bfa9
Chris Thoburn <runspired@gmail.com>
2016-01-16 19:45:05 -0600

    feat(module-import): adds module import exercise

--- app/utils/module-import.js
+++ app/utils/module-import.js
@@ -1,3 +1,10 @@
-export default function moduleImport() {
-  return true;
-}
+/*
+ * You can read about imports here:
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import
+ *
+ * For this exercise, you will need to `import` `spam`, `foo`, `bar` and `baz` from
+ * your module-export exercise.
+ *
+ * ES6 allows you to "re-export" an import.
+ *
+ */
--- tests/unit/utils/module-import-test.js
+++ tests/unit/utils/module-import-test.js
@@ -3,8 +3,9 @@ import { module, test } from 'qunit';
 
 module('Unit | Utility | module import');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let result = moduleImport();
-  assert.ok(result);
+test('The modules correctly re-exports each dependency.', function(assert) {
+  assert.ok(moduleImport.spam && typeof moduleImport.spam === 'function', 'spam was re-exported');
+  assert.ok(moduleImport.foo && typeof moduleImport.foo === 'function', 'foo was re-exported');
+  assert.ok(moduleImport.bar && typeof moduleImport.bar === 'function', 'bar was re-exported');
+  assert.ok(moduleImport.baz && typeof moduleImport.baz === 'function', 'baz was re-exported');
 });
