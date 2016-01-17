e0714391eb4d4ffdb782de3a1f182ab303a9cd28
Chris Thoburn <runspired@gmail.com>
2016-01-16 19:32:57 -0600

    feat(module-export): adds module export tests

--- tests/unit/utils/module-export-test.js
+++ tests/unit/utils/module-export-test.js
@@ -1,10 +1,23 @@
 import moduleExport from '../../../utils/module-export';
+import { foo, bar, baz } from '../../../utils/module-export';
+
 import { module, test } from 'qunit';
 
 module('Unit | Utility | module export');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
+test('The module has a default export', function(assert) {
   let result = moduleExport();
   assert.ok(result);
+});
+
+test('The module exports a function named foo', function(assert) {
+  assert.ok(foo && typeof foo === 'function');
+});
+
+test('The module exports a function named bar', function(assert) {
+  assert.ok(bar && typeof bar === 'function');
+});
+
+test('The module exports a function named baz', function(assert) {
+  assert.ok(bar && typeof bar === 'function');
 });
