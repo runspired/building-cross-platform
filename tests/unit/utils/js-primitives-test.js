78613a5e5e5ca990746a5055af6476d9bb75d214
Chris Thoburn <runspired@gmail.com>
2016-01-18 17:07:18 -0600

    don't look

--- tests/unit/utils/js-primitives-test.js
+++ tests/unit/utils/js-primitives-test.js
@@ -1,4 +1,4 @@
-import jsPrimitives from '../../../utils/js-primitives';
+import { aString, aNull, anUndefined, aNumber, aBoolean, anObject, aFunction } from '../../../utils/js-primitives';
 import { module, test } from 'qunit';
 
 module('Unit | Utility | js primitives');
@@ -8,3 +8,18 @@ test('it works', function(assert) {
   let result = jsPrimitives();
   assert.ok(result);
 });
+
+// export `aString`, set it to a string value.
+
+// export `aNull`, set it to a null value.
+
+// export `anUndefined`, set it to an undefined value.
+
+// export `aNumber`, set it to a Number value.
+
+// export `aBoolean`, set it to a boolean value.
+
+// export `anObject`, set it to be some object.
+
+// export `aFunction`, set it to be some function.
+
