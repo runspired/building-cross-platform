f36bc8d3df8d632a2e8c13eb185d4550c7582161
Chris Thoburn <runspired@gmail.com>
2016-01-20 19:15:25 -0600

    solutions

--- app/utils/conditionals.js
+++ app/utils/conditionals.js
@@ -10,35 +10,63 @@
 /*
  * export a function named `both` which tests whether both of two variables is truthy.
  */
+export function both(a, b) {
+  return a && b;
+}
 
 /*
  * export a function named `either` which tests whether either of two variables is truthy.
  */
+export function either(a, b) {
+  return a || b;
+}
 
 /*
  * export a function named `negative` which tests whether a variable is `false`.
  */
+export function negative(a) {
+  return a === false;
+}
 
 /*
  * export a function named `positive` which tests whether a variable is `true`.
  */
+export function positive(a) {
+  return a === true;
+}
 
 /*
  * export a function named `all` which tests whether all of three variables are truthy.
  */
+export function all(a, b, c) {
+  return a && b && c;
+}
 
 /*
  * export a function named `none` which tests whether all of three variables is falsey
  */
+export function none(a, b, c) {
+  return !a && !b && !c;
+}
 
 /*
  * export a function named `any` which tests whether any of three variables is truthy
  */
+export function any(a, b, c) {
+  return a || b || c;
+}
 
 /*
  * export a function named `isTrue` that returns whether a variable is `true`
  */
+export function isTrue(a) {
+  return positive(a);
+}
 
 /*
- * export a function named `isTruthy` that returns whether a variable is `truthy`
+ * export a function named `isTruthy` that returns `true` if a variable is `truthy`
+ * and `false` otherwise.
  */
+export function isTruthy(a) {
+  return !!a;
+}
