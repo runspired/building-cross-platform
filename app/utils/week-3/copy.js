fee8f8ced4bf2bd8dc7179c8448d741a19a7d71e
Chris Thoburn <runspired@gmail.com>
2016-01-23 15:42:33 -0600

    feat(copy-exercise): implements copy exercise

---
+++ app/utils/week-3/copy.js
@@ -0,0 +1,13 @@
+// export a function named `copy`
+// which takes an object or an array and returns a shallow copy
+// of the object or the array
+
+// a "shallow" copy means that you return a new object/array,
+// with identical keys/indeces to the original pointing at
+// the same references for the value for each key or index
+
+// you can use Object.prototype.toString to detect if something is
+// an object or is an array
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/toString
+
+
