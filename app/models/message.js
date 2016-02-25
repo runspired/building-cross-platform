10645dc802564cd6aef02a13c6da4fca01a1b95a
Chris Thoburn <runspired@gmail.com>
2016-02-24 18:35:33 -0600

    monday

--- app/models/message.js
+++ app/models/message.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   conversation: DS.belongsTo('conversation'),
+  time: DS.attr('date', { defaultValue: (new Date()).toISOString() }),
   user: DS.belongsTo('user'),
   text: DS.attr()
 });
